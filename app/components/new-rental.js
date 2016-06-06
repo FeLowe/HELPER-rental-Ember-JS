d5d15bf42cfdb4a8cebfb3ad545efe9e0cac67c8
Noah Kittleson and Samuel Durfey <samuel@>
2016-06-06 16:20:15 -0700

    You can now update entries, and blank entries are dealt with.

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -9,11 +9,11 @@ export default Ember.Component.extend({
 
     save1() {
       var params = {
-        owner: this.get('owner'),
-        city: this.get('city'),
-        type: this.get('type'),
-        image: this.get('image'),
-        bedrooms: this.get('bedrooms'),
+        owner: this.get('owner') === undefined ? "undefined" : this.get('owner'),
+        city: this.get('city') === undefined ? "undefined" : this.get('city'),
+        type: this.get('type') === undefined ? "undefined" : this.get('type'),
+        image: this.get('image') === undefined ? "undefined" : this.get('image'),
+        bedrooms: this.get('bedrooms') === undefined ? "undefined" : this.get('bedrooms'),
       };
       this.set('addNewRental', false);
       this.sendAction('save2', params);
